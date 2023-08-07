59b3bfd8961e2bf0b3961400f3f6e1b5631bd17b
miyabe403 <108040533+miyabe403@users.noreply.github.com>
2023-08-08 07:34:22 +0900

    [Add] テーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -43,7 +43,7 @@ ActiveRecord::Schema.define(version: 2023_08_07_220614) do
   create_table "post_images", force: :cascade do |t|
     t.string "shop_name"
     t.text "caption"
-    t.integer "user_id"
+    t.integer "user_id" 
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
