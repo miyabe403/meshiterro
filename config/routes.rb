f9637a8a129331b2e11c138aae7d77ce7ddaee17
miyabe403 <108040533+miyabe403@users.noreply.github.com>
2023-08-07 17:43:38 +0000

    [Start] 4-7

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'homes#top' 
+  root to: 'homes#top'
+  get '/homes/about' => 'homes#about', as: 'about'  # 名前付きルート as:オプションを追加「'lists#show'の設定を、listとして利用できる」
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+ 
