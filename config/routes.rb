8d33016e1917fe7fe7213679ac458bba2a278b68
miyabe403 <108040533+miyabe403@users.noreply.github.com>
2023-08-07 17:44:18 +0000

    [Add] Aboutページの作成

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,3 @@ Rails.application.routes.draw do
   get '/homes/about' => 'homes#about', as: 'about'  # 名前付きルート as:オプションを追加「'lists#show'の設定を、listとして利用できる」
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
- 
