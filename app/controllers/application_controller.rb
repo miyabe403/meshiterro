1d28ef6f4616777da85086bd8cd95117d6c78e8e
miyabe403 <108040533+miyabe403@users.noreply.github.com>
2023-08-07 16:35:37 +0000

    [Add] ログアウトリンクを作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,6 @@ class ApplicationController < ActionController::Base
   # configure_permitted_parametersメソッドでは、devise_parameter_sanitizer.permitメソッドを使うことで
   # ユーザー登録(sign_up)の際に、ユーザー名(name)のデータ操作を許可しています。
   def configure_permitted_parameters
-    devise_parameter_sanitizer.parmit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
 end
