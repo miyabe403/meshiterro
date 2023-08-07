7539349e0cd58983ad2676e3836e7d91eb5b5596
miyabe403 <108040533+miyabe403@users.noreply.github.com>
2023-08-08 08:45:57 +0900

    [Add] User モデルに PostImage モデルの関連付けを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   #（パスワードの正確性を検証）（ユーザ登録や編集、削除）
   #（パスワードをリセット）（ログイン情報を保存）（email のフォーマットなどのバリデーション）
+
+  has_many :post_images, dependent: :destroy
+
 end
