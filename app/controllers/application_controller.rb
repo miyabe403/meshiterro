3e909746dee550dc5615ca1c20e22adb136574d8
miyabe403 <108040533+miyabe403@users.noreply.github.com>
2023-08-07 18:10:12 +0000

    [Add] after_sign_in_path_forメソッドでサインイン後にAboutページへ遷移するように設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,11 @@ class ApplicationController < ActionController::Base
   # devise利用の機能（ユーザ登録、ログイン認証など）が使われる前にconfigure_permitted_parametersメソッドが実行されます。
   before_action :configure_permitted_parameters, if: :devise_controller?
   
+  # after_sign_in_path_forはDeviseが用意しているメソッドで、サインイン後にどこに遷移するかを設定しているメソッドです。
+  def after_sign_in_path_for(resource)
+    about_path
+  end
+  
   # protectedは呼び出された他のコントローラからも参照することができます。
   protected
   
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
-  get '/homes/about' => 'homes#about', as: 'about'  # 名前付きルート as:オプションを追加「'lists#show'の設定を、listとして利用できる」
+  get '/homes/about' => 'homes#about', as: 'about'  # 名前付きルート as:オプションを追加「'homes#about'の設定を、aboutとして利用できる」
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
