1573644af7a0baa837e9900b835d991ba0405f42
miyabe403 <108040533+miyabe403@users.noreply.github.com>
2023-08-08 20:25:25 +0900

    [Add] Routing の resources の部分に destroy を追記

--- config/routes.rb
+++ config/routes.rb
@@ -7,3 +7,4 @@ Rails.application.routes.draw do
   get '/homes/about' => 'homes#about', as: 'about'  # 名前付きルート as:オプションを追加「'homes#about'の設定を、aboutとして利用できる」
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+ 
