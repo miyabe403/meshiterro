13c0ac3f7055f1e2ed22e53be62d065cb9bface1
miyabe403 <108040533+miyabe403@users.noreply.github.com>
2023-08-08 15:12:55 +0900

    [Start] 4-11

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -6,4 +6,15 @@ class PostImage < ApplicationRecord
   # PostImage モデルに関連付けられるのは、1 つの User モデルです。
   # このため、単数形の「user」になっている点に注意しましょう。
   belongs_to :user
+  
+  # get_image メソッド =特定の処理を名前で呼び出すことができる
+  # unless = 画像が設定されない場合はimagesに格納されているno_image.jpgという画像をデフォルト画像としてActiveStorageに格納する
+  def get_image
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    image
+  end
+  
 end
