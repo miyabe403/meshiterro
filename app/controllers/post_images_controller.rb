59f37d5409f9f857a19760ce3e5c3ceb6e453b07
miyabe403 <108040533+miyabe403@users.noreply.github.com>
2023-08-08 11:40:07 +0900

    [Add] Routing を追加

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -5,8 +5,8 @@ class PostImagesController < ApplicationController
   
   # 投稿データの保存
   def create
-    @post_image = PostImage.new(post_image_params)
-    @post_image.user_id = current_user.id
+    @post_image = PostImage.new(post_image_params) # 投稿するデータを PostImage モデルに紐づくデータとして保存する準備している
+    @post_image.user_id = current_user.id　# 空のモデルでは、"[モデル名].[カラム名]"という形で、保存する。 current_user は、ログイン中のユーザー情報を取得することができる
     @post_image.save
     redirect_to post_images_path
   end
