f44c22d1dcd6d87bedb8ec6e55f0481d0b4dd625
miyabe403 <108040533+miyabe403@users.noreply.github.com>
2023-08-08 21:22:19 +0900

    [Start] 4-14

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,15 @@ class User < ApplicationRecord
   # User モデルに対して、PostImage モデルが 1:N になるよう関連付けます。
   has_many :post_images, dependent: :destroy
 
+  # profile_imageという名前でActiveStorageでプロフィール画像を保存
+  has_one_attached :profile_image
+  
+  # get_image メソッド =特定の処理を名前で呼び出すことができる 
+  def get_profile_image
+    unless profile_image.attached?
+      file_path = Rails.root.join('app/assets/images/sample-author1.jpg')
+      profile_image.attach(io: File.open(file_path), filename: 'default-image-jpg', content_type: 'image/jpeg')
+    end
+    profile_image.variant(resize_to_limit: [width, height]).processed # 画像サイズの変更を行っています
+  end
 end
