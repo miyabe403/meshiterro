49a61b20f21c045400464d83b301df3d45a8bb29
miyabe403 <108040533+miyabe403@users.noreply.github.com>
2023-08-08 14:42:33 +0900

    [Finish] 4-10

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -7,7 +7,7 @@ class PostImagesController < ApplicationController
   def create
     @post_image = PostImage.new(post_image_params) # 投稿するデータを PostImage モデルに紐づくデータとして保存する準備している
     @post_image.user_id = current_user.id  # 空のモデルでは、"[モデル名].[カラム名]"という形で、保存する。 current_user は、ログイン中のユーザー情報を取得することができる
-    
+     
     @post_image.save
     redirect_to post_images_path
   end
