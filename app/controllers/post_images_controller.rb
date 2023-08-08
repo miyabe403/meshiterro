03599aed721ab699b66ea70fd92ff78f787aa4cd
miyabe403 <108040533+miyabe403@users.noreply.github.com>
2023-08-08 20:21:31 +0900

    [Add] Controller の destroy アクションに記述を追加

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -2,31 +2,33 @@ class PostImagesController < ApplicationController
   def new
     @post_image = PostImage.new
   end
-  
+
   # 投稿データの保存
   def create
     @post_image = PostImage.new(post_image_params) # 投稿するデータを PostImage モデルに紐づくデータとして保存する準備している
     @post_image.user_id = current_user.id  # 空のモデルでは、"[モデル名].[カラム名]"という形で、保存する。 current_user は、ログイン中のユーザー情報を取得することができる
-     
+
     @post_image.save
     redirect_to post_images_path
   end
 
-  def index 
+  def index
     @post_images = PostImage.all
   end
 
   def show
     @post_image = PostImage.find(params[:id])
   end
-  
+
   def destroy
-    
+    post_image = PostImage.find(params[:id])
+    post_image.destroy
+    redirect_to '/post_images'
   end
-  
+
   # 投稿データのストロングパラメータ
-  private 
-  
+  private
+
   def post_image_params
     params.require(:post_image).permit(:shop_name, :image, :caption)
   end
