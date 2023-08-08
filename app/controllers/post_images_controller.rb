0bbaaab5c3025c880eb05294bf65d841616a4ebe
miyabe403 <108040533+miyabe403@users.noreply.github.com>
2023-08-08 14:41:45 +0900

    [Add] 記事を投稿して登録機能を確認

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -6,7 +6,8 @@ class PostImagesController < ApplicationController
   # 投稿データの保存
   def create
     @post_image = PostImage.new(post_image_params) # 投稿するデータを PostImage モデルに紐づくデータとして保存する準備している
-    @post_image.user_id = current_user.id　# 空のモデルでは、"[モデル名].[カラム名]"という形で、保存する。 current_user は、ログイン中のユーザー情報を取得することができる
+    @post_image.user_id = current_user.id  # 空のモデルでは、"[モデル名].[カラム名]"という形で、保存する。 current_user は、ログイン中のユーザー情報を取得することができる
+    
     @post_image.save
     redirect_to post_images_path
   end
@@ -21,6 +22,6 @@ class PostImagesController < ApplicationController
   private 
   
   def post_image_params
-    params_require(:post_image).permit(:shop_name, :image, :caption)
+    params.require(:post_image).permit(:shop_name, :image, :caption)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,6 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   devise_for :users  # devise_for :users は、devise を使用する際に URL として users を含むことを示しています。
   
-  get 'post_images/new'
-  get 'post_images/index'
-  get 'post_images/show'
-  
   resources :post_images, only: [:new, :create, :index, :show] # resources メソッドは、ルーティングを一括して自動生成してくれる機能 onlyオプションを使用することで、生成するルーティングを限定することができます。
    
   get '/homes/about' => 'homes#about', as: 'about'  # 名前付きルート as:オプションを追加「'homes#about'の設定を、aboutとして利用できる」
