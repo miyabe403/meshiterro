0396c9d85d39f26f15f58475c6d9cdacfde9d77d
miyabe403 <108040533+miyabe403@users.noreply.github.com>
2023-08-08 11:24:55 +0900

    [Add] 投稿データを保存するためにcreateアクションを作成

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -1,11 +1,26 @@
 class PostImagesController < ApplicationController
   def new
-    @post_image = Post.new
+    @post_image = PostImage.new
+  end
+  
+  # 投稿データの保存
+  def create
+    @post_image = PostImage.new(post_image_params)
+    @post_image.user_id = current_user.id
+    @post_image.save
+    redirect_to post_images_path
   end
 
   def index
   end
 
   def show
+  end
+  
+  # 投稿データのストロングパラメータ
+  private
+  
+  def post_image_params
+    params_require(:post_image).permit
   end
 end
