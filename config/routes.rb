af88eb4cc9c27ea5fe0d07b7e2a5fa72f6994b8e
miyabe403 <108040533+miyabe403@users.noreply.github.com>
2023-08-08 09:27:24 +0900

    [Finish] 4-9

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   get 'post_images/show'
   
   resources :post_images, only: [:new, index, :show] # resources メソッドは、ルーティングを一括して自動生成してくれる機能 onlyオプションを使用することで、生成するルーティングを限定することができます。
-  
+   
   get '/homes/about' => 'homes#about', as: 'about'  # 名前付きルート as:オプションを追加「'homes#about'の設定を、aboutとして利用できる」
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
