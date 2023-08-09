931f9506a11a678545533d1e5b382495c19d5348
miyabe403 <108040533+miyabe403@users.noreply.github.com>
2023-08-09 20:07:58 +0900

    [Add] 投稿画像の詳細画面で、コメントを投稿できるように記述を追加

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -18,7 +18,7 @@ class PostImagesController < ApplicationController
 
   def show
     @post_image = PostImage.find(params[:id])
-    @post_comment = PostComment.new
+    @post_comment = PostComment.new  # コメントを投稿するためのインスタンス変数を定義
   end
 
   def destroy
