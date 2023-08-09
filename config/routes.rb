c45081093570bd94fa7cb71d5289a0a8805c97f9
miyabe403 <108040533+miyabe403@users.noreply.github.com>
2023-08-09 19:01:54 +0900

    [Add] コメント投稿機能を作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users  # devise_for :users は、devise を使用する際に URL として users を含むことを示しています。
   root to: 'homes#top'
   get '/homes/about' => 'homes#about', as: 'about'  # 名前付きルート as:オプションを追加「'homes#about'の設定を、aboutとして利用できる」
-  resources :post_images, only: [:new, :create, :index, :show, :destroy] # resources メソッドは、ルーティングを一括して自動生成してくれる機能 onlyオプションを使用することで、生成するルーティングを限定することができます。
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do # resources メソッドは、ルーティングを一括して自動生成してくれる機能 onlyオプションを使用することで、生成するルーティングを限定することができます。
+    resources :post_comments, only: [:create] # コメントは、投稿画像に対してコメントされます。このため、post_commentsは、post_imagesに結びつきます。 以下のように親子関係になります。
+  end
   resources :users, only: [:show, :edit, :update] # resourcesとonlyを使って、show, editのアクションのみ追加 
 end
  
