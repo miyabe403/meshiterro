502cdcb2215d0005b677c45907590c21963ce304
miyabe403 <108040533+miyabe403@users.noreply.github.com>
2023-08-09 18:48:34 +0900

    [Add] UserモデルとPostCommentモデルを関連付ける（PostCommentモデルへの記述）

--- app/models/post_comment.rb
+++ app/models/post_comment.rb
@@ -1,2 +1,13 @@
 class PostComment < ApplicationRecord
+  
+  # PostComment モデルに対して、User モデルとの関係性を追加していきます。
+  # PostComment モデルに関連付けられるのは、1 つの User モデルです。
+  # このため、単数形の「user」になっている点に注意しましょう。
+  belongs_to :user
+  
+  # PostComment モデルに対して、PostImage モデルとの関係性を追加していきます。
+  # PostComment モデルに関連付けられるのは、1 つの PostImage モデルです。
+  # このため、単数形の「post_image」になっている点に注意しましょう。
+  belongs_to :post_image
+  
 end
