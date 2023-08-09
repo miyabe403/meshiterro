a61d36e60c406b467b50d638d126b6febc942f37
miyabe403 <108040533+miyabe403@users.noreply.github.com>
2023-08-09 18:40:09 +0900

    [Add] UserモデルとPostCommentモデルを関連付ける（Userモデルへの記述）

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -7,6 +7,8 @@ class PostImage < ApplicationRecord
   # このため、単数形の「user」になっている点に注意しましょう。
   belongs_to :user
   
+  
+  
   # get_image メソッド =特定の処理を名前で呼び出すことができる
   # unless = 画像が設定されない場合はimagesに格納されているno_image.jpgという画像をデフォルト画像としてActiveStorageに格納する
   def get_image
