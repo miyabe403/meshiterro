913f52c67087e6f60cc9a57c37ed9ebca8c1eda3
miyabe403 <108040533+miyabe403@users.noreply.github.com>
2023-08-09 20:29:26 +0900

    [Add] コメント削除の Routing を設定

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   get '/homes/about' => 'homes#about', as: 'about'  # 名前付きルート as:オプションを追加「'homes#about'の設定を、aboutとして利用できる」
   resources :post_images, only: [:new, :create, :index, :show, :destroy] do # resources メソッドは、ルーティングを一括して自動生成してくれる機能 onlyオプションを使用することで、生成するルーティングを限定することができます。
-    resources :post_comments, only: [:create] # コメントは、投稿画像に対してコメントされます。このため、post_commentsは、post_imagesに結びつきます。 以下のように親子関係になります。
+    resources :post_comments, only: [:create, :destroy] # コメントは、投稿画像に対してコメントされます。このため、post_commentsは、post_imagesに結びつきます。 以下のように親子関係になります。
   end
   resources :users, only: [:show, :edit, :update] # resourcesとonlyを使って、show, editのアクションのみ追加 
 end
