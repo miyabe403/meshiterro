5255c91d4ff4c3ebff4219c0a622c91883485b96
miyabe403 <108040533+miyabe403@users.noreply.github.com>
2023-08-09 11:15:43 +0900

    [Add] Controller の edit アクションに記述を追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @post_images = @user.post_images # 特定のユーザ（@user）に関連付けられた投稿全て（.post_images）を取得し @post_imagesに渡す処理 
   end
-
+ 
   def edit
     @user = User.find(params[:id])
   end
