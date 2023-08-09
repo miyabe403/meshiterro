20261eb5ecadcb126c24e23873aa1200bba68d93
miyabe403 <108040533+miyabe403@users.noreply.github.com>
2023-08-09 19:27:24 +0900

    [Add] createアクション内にコメントを作成し、データベースに保存する記述を追加

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -1,2 +1,10 @@
 class PostCommentsController < ApplicationController
+  
+  def create
+    post_image = PostImage.find(params[:post_image_id])
+    comment = current_user.postcomments.new(post_comment_params) #カレントユーザーのidをコメントのユーザーidに紐づけ
+    comment.post_image_id = post_image.id
+    comment.save
+    redirect_to post_image_path(post_image)
+  end
 end
