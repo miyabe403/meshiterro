22b114df81fdcccb5c7ff3b089d8e9a6979c76ed
miyabe403 <108040533+miyabe403@users.noreply.github.com>
2023-08-09 13:19:10 +0900

    [Add] Routing の resources の部分に update を追記

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   get '/homes/about' => 'homes#about', as: 'about'  # 名前付きルート as:オプションを追加「'homes#about'の設定を、aboutとして利用できる」
   resources :post_images, only: [:new, :create, :index, :show, :destroy] # resources メソッドは、ルーティングを一括して自動生成してくれる機能 onlyオプションを使用することで、生成するルーティングを限定することができます。
-  resources :users, only: [:show, :edit] # resourcesとonlyを使って、show, editのアクションのみ追加 
+  resources :users, only: [:show, :edit, :update] # resourcesとonlyを使って、show, editのアクションのみ追加 
 end
  
