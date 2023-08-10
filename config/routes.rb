cc3cd448bfb9b498f5c34d06edd9f3022db33a2a
miyabe403 <108040533+miyabe403@users.noreply.github.com>
2023-08-10 09:56:03 +0900

    [Add] いいね機能の Routing を設定

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   get '/homes/about' => 'homes#about', as: 'about'  # 名前付きルート as:オプションを追加「'homes#about'の設定を、aboutとして利用できる」
   resources :post_images, only: [:new, :create, :index, :show, :destroy] do # resources メソッドは、ルーティングを一括して自動生成してくれる機能 onlyオプションを使用することで、生成するルーティングを限定することができます。
+    resources :favorites, only: [:create, :destroy]
     resources :post_comments, only: [:create, :destroy] # コメントは、投稿画像に対してコメントされます。このため、post_commentsは、post_imagesに結びつきます。 以下のように親子関係になります。
   end
   resources :users, only: [:show, :edit, :update] # resourcesとonlyを使って、show, editのアクションのみ追加 
