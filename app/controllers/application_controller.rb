0d072b75bae7322c79d72675a1a4416a07283cfd
miyabe403 <108040533+miyabe403@users.noreply.github.com>
2023-08-10 17:34:59 +0900

    [Start] 4-21

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!, except: [:top]
   # devise利用の機能（ユーザ登録、ログイン認証など）が使われる前にconfigure_permitted_parametersメソッドが実行されます。
   before_action :configure_permitted_parameters, if: :devise_controller?
   
