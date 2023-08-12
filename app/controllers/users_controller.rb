787dc4331fa84630bf8104077cc6d1ef9e855863
miyabe403 <108040533+miyabe403@users.noreply.github.com>
2023-08-12 18:49:16 +0900

    [Add] before_actionでアクション実行前に判断

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,19 @@
 class UsersController < ApplicationController
+  # メソッドとして処理をまとめたことで、before_actionを使用することができます。
+  # before_actionは、コントローラーで各アクションを実行する前に実行したい処理を指定することができるメソッドです。
+  # 実行したい処理は、メソッドとしてまとめることで実行できます。
+  before_action :is_matching_login_user, only: [:edit, :update]
+  
   def show
     @user = User.find(params[:id])
     @post_images = @user.post_images.page(params[:page]) # 特定のユーザ（@user）に関連付けられた投稿全て（.post_images）を取得し @post_imagesに渡す処理 
   end
  
   def edit 
-    is_matching_login_user  # メソッドで処理をまとめる
-      
     @user = User.find(params[:id])
   end
   
   def update
-    is_matching_login_user  # メソッドで処理をまとめる
-    
     @user = User.find(params[:id])
     @user.update(user_params)
     redirect_to user_path(@user.id)
@@ -29,6 +30,7 @@ class UsersController < ApplicationController
     user = User.find(params[:id])  # 1. URLに含まれるユーザーidをparams[:id]で取得
     unless user.id == current_user.id  # 2. ログインしているユーザーのidをcurrent_user.idで取得
       redirect_to post_images_path  # 3. 1と2のidが一致していなかった場合、 投稿一覧にリダイレクトする
+    end
   end
 end
 
